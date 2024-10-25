773b6086b8da2bc9bbd3be7b9dccfd2c871d5685
DamianMoris0 <Damian.Moris@brightest.be>
2024-10-25 16:27:57 +0200

    added possibility to toggle mqtt publish and json file write + added infinite loop

--- lib/sensor.h
+++ lib/sensor.h
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 
-#define BMP180              0
-#define SEND_DATA_INTERVAL  1000000    // time in µs, defines the time intervals after which to send the json data (60.000.000 µs == 1 min)
-#define SAMPLE_TIME         100000     // time in µs, dont exceed SEND_DATA_INTERVAL
+#define BMP180                          0
+#define SEND_JSON_DATA_FILE_INTERVAL    1000000 // Time in µs, defines the time intervals after which to send the json data (60.000.000 µs == 1 min)
+#define SAMPLE_TIME                     100000  // Time in µs, dont exceed SEND_JSON_DATA_FILE_INTERVAL
+#define write_to_json_file_flag         0       // Set to 1 to write to json file, else set 0
+#define publish_json_on_mqtt_topic_flag 1       // Set to 1 to publish json on mqtt topic, else set 0
 
 struct SensorValues {
     int type;
--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include "lib/sensor.h"
 #include "lib/mqtt.h"
 
+#define MAX_FAULTY_SENSOR_READINGS 60
+
 int main()
 {
 	/* Initialise variables */
@@ -20,6 +22,7 @@ int main()
 	struct SensorValues sensorValues;
 	sensorValues.type = BMP180;
 	sensorValues.id = "Sensor_1";
+	int loopCounter = 0;
 
 	/* MQTT variables */
 	MQTTClient client;
@@ -43,31 +46,46 @@ int main()
 		return 1;
 	}
 
-	/* Read sensor data and write to file */
-	int loopAmount = SEND_DATA_INTERVAL / SAMPLE_TIME; // Specifies the amount of loops needed to send full json data file every SEND_DATA_INTERVAL seconds, with data samples every SAMPLE_TIME seconds
-	for (int i = 0; i < loopAmount; i++) {
-		/* Convert pressure data into hPa + get one decimal place accuracy on values */
-		sensorValues.temperature = round(bmp180_temperature(sensor) * 100) / 100.0;
-		sensorValues.pressure = round(bmp180_pressure(sensor) / 10) / 10;
+	/* Read sensor data and publish on mqtt topic + write to file if specified */
+	int loopAmount = SEND_JSON_DATA_FILE_INTERVAL / SAMPLE_TIME; // Calculate amount of loops needed for specified SEND_JSON_DATA_FILE_INTERVAL per SAMPLE_TIME
+	for (;;) {
+		for (int i = 0; i < loopAmount; i++) {
+			/* Convert pressure data into hPa + get one decimal place accuracy on values */
+			sensorValues.temperature = round(bmp180_temperature(sensor) * 100) / 100.0;
+			sensorValues.pressure = round(bmp180_pressure(sensor) / 10) / 10;
 
-		/* Check data bounds of sensor data values */
-		if (checkDataBounds(sensorValues.temperature, sensorValues.pressure)) {
-			printf("Sensor data out of bounds\n");
-			i--; // Decrement i and continue to read again, so json file will be the same size
-			continue;
+			/* Check data bounds of sensor data values */
+			if (checkDataBounds(sensorValues.temperature, sensorValues.pressure)) {
+				printf("Sensor data out of bounds\n");
+				i--; // Decrement i and continue to read again, so json file will be the same size
+				loopCounter++;
+				if (loopCounter >= MAX_FAULTY_SENSOR_READINGS) {
+					printf("Sensor possibly broken, %d readins were out of bounds\n", MAX_FAULTY_SENSOR_READINGS);
+					break;
+				}
+				continue;
+			}
+			loopCounter = 0;
+
+			/* Get raw time and convert to readable time + remove '\n' from last char in array */
+			time(&rawtime);
+			timeinfo = localtime(&rawtime);
+			sensorValues.time = asctime(timeinfo);
+			sensorValues.time[strlen(sensorValues.time) - 1] = '\0';
+
+			/* Publish MQTT message on topic + write sensor data to file for potential future use, depending on if corresponding flags are turned on/off */
+			jsonPayloadMessage = createJsonFromSensorData(&sensorValues);
+			if (publish_json_on_mqtt_topic_flag) {
+				publishMessage(client, jsonPayloadMessage);
+			}
+			if (write_to_json_file_flag) {
+				writeSensorDataToFile(&sensorDataFile, jsonPayloadMessage);
+			}
+			usleep(SAMPLE_TIME); // Time in µs
 		}
-
-		/* Get raw time and convert to readable time + remove '\n' from last char in array */
-		time(&rawtime);
-		timeinfo = localtime(&rawtime);
-		sensorValues.time = asctime(timeinfo);
-		sensorValues.time[strlen(sensorValues.time) - 1] = '\0';
-
-		/* Publish MQTT message on topic + write sensor data to file for potential future use */
-		jsonPayloadMessage = createJsonFromSensorData(&sensorValues);
-		publishMessage(client, jsonPayloadMessage);
-		writeSensorDataToFile(&sensorDataFile, jsonPayloadMessage);
-		usleep(SAMPLE_TIME); // Time in µs
+		if (write_to_json_file_flag) {
+			break; // If data is written to file break loop to be able to access data.json file, can be changed in the future for other purposes
+		}
 	}
 
 	/* Close file and bmp180 */
